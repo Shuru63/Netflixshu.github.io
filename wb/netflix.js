e2ebe38566ee9ed04466289508901ad81123279b
Shuru63 <121496598+Shuru63@users.noreply.github.com>
2023-08-07 21:47:17 +0530

    Add files via upload

---
+++ wb/netflix.js
@@ -0,0 +1,24 @@
+let question=document.getElementById("answer1");
+let question1=document.getElementById("answer2");
+let question2=document.getElementById("answer3");
+let question3=document.getElementById("answer4");
+let question5=document.getElementById("answer5");
+let question6=document.getElementById("answer6");
+ function openanswer1(){
+    question.classList.toggle("open_answer");
+ }
+ function openanswer2(){
+    question1.classList.toggle("open_answer");
+ }
+ function openanswer3(){
+   question2.classList.toggle("open_answer");
+}
+function openanswer4(){
+   question3.classList.toggle("open_answer");
+}
+function openanswer5(){
+   question5.classList.toggle("open_answer");
+}
+function openanswer6(){
+   question6.classList.toggle("open_answer");
+}
